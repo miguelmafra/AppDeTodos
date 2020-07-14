35226b90f5dd84fe379d72f335d7bca59bda8d0a
miguelmafra <miguelml159357@gmail.com>
2020-07-14 13:24:01 -0300

    Adicionar e excluir todos feito.

--- todos.js
+++ todos.js
@@ -7,3 +7,44 @@ var todos = [
     'Estudar Javascript',
     'Acessar comunidade da Rocketseat'
 ];
+
+
+function renderTodos() {
+    listElement.innerHTML = '';
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+        var linkText = document.createTextNode('Excluir')
+
+        linkElement.appendChild(linkText);
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    }
+}
+
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
+
+
+function deleteTodo(pos){
+    todos.splice(pos, 1);
+    renderTodos();
+}
